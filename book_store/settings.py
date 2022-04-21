b66f9224ee6ed7d66b5605859284afb4d74544c0
Cannon-Cole <colecannonschool@gmail.com>
2022-04-20 20:22:32 -0600

    Added book model and its migration

--- book_store/settings.py
+++ book_store/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'book_outlet',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
