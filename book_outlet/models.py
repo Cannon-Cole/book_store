b3a1a9507e9f82bef11b7c45265a10c5dfecab13
Cannon-Cole <colecannonschool@gmail.com>
2022-06-11 05:44:53 -0600

    Configured django admin

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -11,14 +11,11 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)
+    slug = models.SlugField(default="", blank=True,
+                            editable=True, null=False, db_index=True)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
