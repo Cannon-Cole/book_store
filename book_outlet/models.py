d507455984d0e89ffc6e05da57d1e5d4c68fd10f
Cannon-Cole <colecannonschool@gmail.com>
2022-06-21 21:19:35 -0600

    Added author model

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,11 +5,16 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True,
                             editable=True, null=False, db_index=True)
